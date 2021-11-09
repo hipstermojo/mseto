6a4d703deec455f2580c05472dc17fd22863b4bd
Kenneth Gitere <gitere81@gmail.com>
2021-11-09 15:46:39 +0300

    dev: add Netlify adapter for development previews

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,6 +12,7 @@ const config = {
 	],
 
 	kit: {
+		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}

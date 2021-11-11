d4fca6303705b6bd2aabe93eab069eabdcb2da37
Kenneth Gitere <gitere81@gmail.com>
2021-11-11 14:44:52 +0300

    refactor: create `generatePuzzle` function to simplify puzzle creation from words

--- frontend/src/utils/puzzle.ts
+++ frontend/src/utils/puzzle.ts
@@ -1,15 +1,36 @@
 import type { PuzzleTile } from './types';
 
-export const generatePuzzle = (words: string[]): PuzzleTile[][] =>
-	[
-		['t', 'u', 'm', 'w'],
-		['p', 'a'],
-		['a', 'm', 'd', 'k', 'r'],
-		['h', 'u', 'n', 'a', 'e'],
-		['f', 'h', 'g', 't', 'z'],
-		['o', 'i', 'a', 'e']
-	].map((col) =>
-		col.map((letter) => {
+export const generatePuzzle = (words: string[]): PuzzleTile[][] => {
+	let columns = words.reduce((result: Set<string>[], word) => {
+		for (let i = 0; i < word.length; i++) {
+			const letter = word[i];
+			if (result[i]) {
+				result[i].add(letter);
+			} else {
+				result.push(new Set([letter]));
+			}
+		}
+		return result;
+	}, []);
+
+	return columns.map((col) =>
+		shuffle([...col]).map((letter: string) => {
 			return { letter, done: false };
 		})
 	);
+};
+
+function shuffle(letters: string[]): string[] {
+	// Obtained from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+	let currentIndex = letters.length,
+		randomIndex: number;
+
+	while (currentIndex != 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		[letters[currentIndex], letters[randomIndex]] = [letters[randomIndex], letters[currentIndex]];
+	}
+
+	return letters;
+}

441537b5148b787dd078ced324c82d10add9e9bb
Kenneth Gitere <gitere81@gmail.com>
2021-12-28 08:54:19 +0300

    refactor: move puzzle creation code to utils

--- frontend/src/utils/puzzle.ts
+++ frontend/src/utils/puzzle.ts
@@ -1,4 +1,4 @@
-import type { PuzzleTile } from './types';
+import type { Puzzle, PuzzleTile } from './types';
 
 export const generatePuzzle = (words: string[]): PuzzleTile[][] => {
 	let columns = words.reduce((result: Set<string>[], word) => {
@@ -34,3 +34,79 @@ function shuffle(letters: string[]): string[] {
 
 	return letters;
 }
+
+export const extraWords = [
+	'ndama',
+	'ndume',
+	'ndumo',
+	'nduni',
+	'nauni',
+	'nasia',
+	'nusia',
+	'ndeme',
+	'nunia',
+	'nusia',
+	'nauni',
+	'namna',
+	'kemia',
+	'kania',
+	'kesha',
+	'kauka',
+	'kasha',
+	'kasma',
+	'kasia',
+	'kunia',
+	'kuuni',
+	'deuka',
+	'deski',
+	'desia',
+	'dumaa',
+	'dumia',
+	'mesha',
+	'mvuko',
+	'mvumo',
+	'mdeke',
+	'mdeni',
+	'mamia',
+	'maana',
+	'mauko',
+	'maume',
+	'masia',
+	'teuka',
+	'tania',
+	'tamka',
+	'tamko',
+	'tamia',
+	'tauni',
+	'tasia',
+	'tunia',
+	'tumia',
+	'tuama',
+	'tusia'
+];
+
+export const createPuzzle = (
+	coreWords: string[] = [],
+	extraWords: string[] = [],
+	cols: string[][] = []
+): Puzzle => {
+	const today = new Date();
+	return {
+		id: today.toDateString(),
+		cols: cols.map((col) => col.map((letter) => ({ letter, done: false }))),
+		duration: 0,
+		solutions: {
+			core: new Set(coreWords),
+			extra: new Set(extraWords)
+		},
+		foundWords: new Set(),
+		rowPositions: cols.reduce((acc: number[], cur) => {
+			acc.push(Math.floor(cur.length / 2));
+			return acc;
+		}, []),
+		startedAt: null,
+		wordExists: false,
+		tilesCompleted: 0,
+		totalTiles: cols.reduce((acc, cur) => acc + cur.length, 0)
+	};
+};
